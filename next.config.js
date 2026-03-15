4276d0aae2ee1837f1bf9fea7aa51784ad35f57e
Francisco Veiga <francisco.veiga@kaizen360aceleradora.com.br>
2026-03-15 09:25:24 -0300

    revert: remove redirect / → /diagnostico

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/diagnostico',
-        permanent: false,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
