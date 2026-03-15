fdbb1acb6c094213578c1ef100dff8a4ec763a81
Francisco Veiga <francisco.veiga@kaizen360aceleradora.com.br>
2026-03-15 09:22:35 -0300

    redirect: / → /diagnostico

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/diagnostico',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
